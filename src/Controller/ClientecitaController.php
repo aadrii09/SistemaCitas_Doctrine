c9c4b7bc3e35f47f80c5ee244d5e35d03ca30031
aadrii09 <adriancastrobeiro@gmail.com>
2025-02-12 17:42:12 +0100

    listar hecho, pero no detecta los valores introducidos en la bbdd

--- src/Controller/ClientecitaController.php
+++ src/Controller/ClientecitaController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Cliente;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ClientecitaController extends AbstractController
 {
-    #[Route('/clientecita', name: 'app_clientecita')]
-    public function index(): Response
+    #[Route('/list', name: 'listar')]
+    public function listarCliente(EntityManagerInterface $entityManagerInterface ): Response
     {
+
+        $listaClientes=$entityManagerInterface->getRepository(Cliente::class)->findAll();
+        
+        $cuenta=count($listaClientes);
+
         return $this->render('clientecita/index.html.twig', [
-            'controller_name' => 'ClientecitaController',
+            'listaClientes'=>$listaClientes,
+            'cuenta'=>$cuenta,
         ]);
     }
 }
