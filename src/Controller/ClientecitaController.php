c84c0ba3b2eb12ba40e64efe846f356f4593756e
aadrii09 <adriancastrobeiro@gmail.com>
2025-02-18 16:24:37 +0100

    empezando con el loggin y el security

--- src/Controller/ClientecitaController.php
+++ src/Controller/ClientecitaController.php
@@ -10,7 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 final class ClientecitaController extends AbstractController
 {
     #[Route('/list', name: 'listar')]
